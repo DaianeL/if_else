4e9d83d662fb6ecfd9533ae98a1cbe9be96e76ee
Daiane Lima <lima.dai@outlook.com>
2023-05-16 15:49:02 -0300

    Atividades: Atualizadas

---
+++ script.js
@@ -0,0 +1,27 @@
+let aluguel = 1.200; //Aparecerá "Casa alugada"
+
+//aluguel = 3.000; //Aparecerá "Procurando casa para alugar"
+
+if (aluguel <= 1.500) {
+    console.log("Alugada")
+} else {
+    console.log("Procurando casa para alugar")
+}
+
+let dataDeNascimento = 10;
+if (dataDeNascimento >10) {
+    console.log("Seu aniversário já passou")
+} else {
+    console.log("É o mês do seu aniversário")
+}
+
+let produtoLista = ("Banana", "Morango","Café","Leite")
+if (produtoLista = "Café")
+console.log("Faltam produtos da lista") 
+
+let number = 10 / 2;
+if (number <=7) {
+    console.log(false)
+} else {
+    console.log(true)
+}
